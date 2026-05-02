3cdd48a3d48588aaf093350d7a4eb9cca566bbe0
Souvik Mahajan <souvikmahajan759@gmail.com>
2026-05-02 12:36:34 +0530

    Add files via upload

--- D1.java
+++ D1.java
@@ -1,27 +1,31 @@
-package DependencyPractise;
+package DataDrivenTesting;
 import java.util.*;
+
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 import java.io.*;
-import org.testng.*;
-import org.testng.annotations.*;
-public class D1 {
-	@Test(priority=1)
-void openapp() {
-	Assert.assertTrue(true);
+
+public class Data1{
+public static void main(String args[])throws FileNotFoundException,IOException {
+	FileInputStream file=new FileInputStream("C:\\Users\\2484105\\eclipse-workspace\\InterimSeleniumPractise\\PractiseData\\Book1.xlsx");
+	XSSFWorkbook workbook=new XSSFWorkbook(file);
+	XSSFSheet sheet=workbook.getSheet("Sheet1");
+	int totalrows=sheet.getLastRowNum();
+	int totalcells=sheet.getRow(0).getLastCellNum();
+	System.out.println("Total rows:"+totalrows);
+	System.out.println("Total cells:"+totalcells);
+	for(int r=0;r<=totalrows;r++) {
+		XSSFRow currentrow=sheet.getRow(r);
+		for(int c=0;c<totalcells;c++) {
+			XSSFCell cell=currentrow.getCell(c);
+			System.out.print(cell.toString()+"\t ");
+		}
+		System.out.println();
+	}
+	workbook.close();
+	file.close();
 }
-	@Test(priority=2,dependsOnMethods= {"openapp"})
-	void login() {
-		Assert.assertTrue(true);
-	}
-	@Test(priority=3,dependsOnMethods= {"login"})
-	void search() {
-		Assert.assertTrue(false);
-	}
-	@Test(priority=4,dependsOnMethods= {"login","search"})
-	void advsearch() {
-	Assert.assertTrue(true);	
-	}
-	@Test(priority=5,dependsOnMethods= {"login"})
-	void logout() {
-		Assert.assertTrue(true);
-	}
 }
